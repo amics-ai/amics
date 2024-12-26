48fee4204f066e85a3ca93e33b96e81790ac084c
Álvaro <alvarofragosoc@hotmail.com>
2024-12-26 07:12:51 +0100

    small changes

--- frontend/landing/src/db/supabase.js
+++ frontend/landing/src/db/supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
         

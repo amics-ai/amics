bb0b2534b6ac4daa625963db9f311012a971e6a3
Álvaro <alvarofragosoc@hotmail.com>
2024-12-26 07:41:23 +0100

    static

--- frontend/landing/astro.config.mjs
+++ frontend/landing/astro.config.mjs
@@ -4,17 +4,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
-  output: 'server'
-});
+  })
